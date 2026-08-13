d53381132e6aa95e7be7fb50bca6507dfcbacae1
2039724976-hue <2039724976@qq.com>
2026-08-13 11:41:22 +0800

    Deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // Service Worker - 个人效率工作台 PWA (米菲兔风格 v2)
-const CACHE_NAME = 'miffy-workbench-v10';
+const CACHE_NAME = 'miffy-workbench-v11';
 const CACHE_FILES = [
   './',
   './index.html',
-  './css/styles.css?v=9',
+  './css/styles.css?v=10',
   './js/storage.js?v=9',
   './js/today-in-history.js?v=9',
   './js/daily-why.js?v=9',
